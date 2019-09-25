d356c404c1898b3fbbe5952f2ff0ee9638ffa397
AnderGonzalezA <agonzalezam18dw@ikzubirimanteo.com>
2019-09-25 09:09:15 +0200

    Ejercicios PHP bloque 1 terminado,v2

--- Ejercicio8.php
+++ Ejercicio8.php
@@ -1,27 +1,14 @@
 <?php
 	$baseImpar=7;
-	$numeroDeEstrellas=1;
-	$numeroDeEspacios=$baseImpar-1;
-	/*while ($numeroDeEstrellas<=$baseImpar){
-		for ($j=0;$j<$numeroDeEspacios;$j++){
+	$numeroDeEspacios=($baseImpar-1)/2;
+	for ($numeroDeEstrellas=1; $numeroDeEstrellas<=$baseImpar; $numeroDeEstrellas=$numeroDeEstrellas+2){
+		for ($j=0; $j<$numeroDeEspacios; $j++){
 			echo("&nbsp;");
 		}
 		for ($i=0; $i<$numeroDeEstrellas; $i++){
 			echo("*");
 		}
 		echo("<br>");
-		$numeroDeEstrellas=$numeroDeEstrellas+2;
-		$numeroDeEspacios=$numeroDeEspacios-2;
-	}*/
-	for ($numeroDeEstrellas=1;$numeroDeEstrellas<=$baseImpar;$numeroDeEstrellas+2){
-		for ($j=0;$j<$numeroDeEspacios;$j++){
-			echo("&nbsp;");
-		}
-		for ($i=0; $i<$numeroDeEstrellas; $i++){
-			echo("*");
-		}
-		echo("<br>");
-		$numeroDeEstrellas=$numeroDeEstrellas+2;
-		$numeroDeEspacios=$numeroDeEspacios-2;
+		$numeroDeEspacios--;
 	}
 ?>
